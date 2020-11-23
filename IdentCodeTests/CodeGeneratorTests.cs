e1af9b3eb3bb75d2c6ad0a1c3698ed68ae4a4a84
Yegor <yegork@me.com>
2020-11-23 12:35:18 +0300

    Add tests

---
+++ IdentCodeTests/CodeGeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ICG = IdentCodes.IdentCodeGenerator;
+
+namespace IdentCodeTests
+{
+    [TestClass]
+    public class CodeGeneratorTests
+    {
+        private void TestMethod(string matStr, int expectedCode)
+        {
+            var code = ICG.GetMaterialCode(matStr);
+            Assert.AreEqual(code, expectedCode);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var matStr = "C345";
+            var expectedCode = 1;
+            TestMethod(matStr, expectedCode);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var matStr = "C355";
+            var expectedCode = 0;
+            TestMethod(matStr, expectedCode);
+        }
+    }
+}
