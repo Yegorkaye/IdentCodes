271f0926f1f086dc6a48fa0b7b9e361bc9bed316
Yegor <yegork@me.com>
2020-11-23 14:35:21 +0300

    Add test cases

--- IdentCodeTests/CodeGeneratorTests.cs
+++ IdentCodeTests/CodeGeneratorTests.cs
@@ -28,5 +28,53 @@ namespace IdentCodeTests
             var expectedCode = 0;
             TestMethod(matStr, expectedCode);
         }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var matStr = "C355Б-Z35";
+            var expectedCode = 6;
+            TestMethod(matStr, expectedCode);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var matStr = "C345Б-Z35";
+            var expectedCode = 1;
+            TestMethod(matStr, expectedCode);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var matStr = "S420-4-AB-Z35";
+            var expectedCode = 2;
+            TestMethod(matStr, expectedCode);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var matStr = "API5L-Z35";
+            var expectedCode = 3;
+            TestMethod(matStr, expectedCode);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            var matStr = "S355";
+            var expectedCode = 4;
+            TestMethod(matStr, expectedCode);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            var matStr = "C255";
+            var expectedCode = 5;
+            TestMethod(matStr, expectedCode);
+        }
     }
 }
